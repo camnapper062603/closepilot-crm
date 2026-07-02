49c5d331c1584c35fc7e1cd57d0feccaef74beaf
Cameron <camnapper062603@gmail.com>
2026-07-02 16:02:14 -0500

    Remove unused Vercel enrichment function

--- api/business-enrichment.js
+++
@@ -1,5 +0,0 @@
-import { handleBusinessEnrichmentRequest } from "../business-enrichment-service.js";
-
-export default function handler(request, response) {
-  return handleBusinessEnrichmentRequest(request, response);
-}
